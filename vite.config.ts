d073c2d4cb8db852265ff5a37178c89d593d91d5
Liubava <liubava.k@bk.ru>
2024-12-20 02:19:14 +0300

    edited config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/newPortfolio/' : '/',
   root: './',
   build: {
     outDir: './dist',
